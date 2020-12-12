25a09e92526c056969029f937bb69b43def4ff60
iloveusername <bchasestair@gmail.com>
2020-12-12 13:26:09 -0800

    Adds in fire bullets

--- Scripts/FireBullet.gd
+++ Scripts/FireBullet.gd
@@ -4,20 +4,15 @@ extends RigidBody2D
 # Declare member variables here. Examples:
 # var a = 2
 var death = 0
-
+onready var fireAnim = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _physics_process(delta):
+	fireAnim.play("Fireball")
 	death += 1
-	if death > 200:
+	if death > 100:
 		queue_free()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_Area2D_body_entered(body):
-	if "EnemyFire" in body.name:
+	if "Player" in body.name:
 		queue_free()
