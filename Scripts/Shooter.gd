15153ccaa2a3db7ba992d3c48a5043c22de9dd8b
iloveusername <bchasestair@gmail.com>
2020-12-12 14:06:22 -0800

    More Stuff

--- Scripts/Shooter.gd
+++ Scripts/Shooter.gd
@@ -15,7 +15,7 @@ func _physics_process(delta):
 func shoot():
 	var bulletInstance = firebullet.instance()
 	var Player = get_parent().get_parent().get_node("Player")
-	bulletInstance.position = get_global_position() + Vector2(15,-15)
+	bulletInstance.position = get_global_position() + Vector2(0,-25)
 	look_at(Player.position)
 	bulletInstance.apply_impulse(Vector2(0,0),Vector2(bulletSpeed, 0).rotated(rotation))
 	get_tree().get_root().call_deferred("add_child",bulletInstance)
