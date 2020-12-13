b7ae396d0adc24dcbf942efe596686cd0b2c96cf
iloveusername <bchasestair@gmail.com>
2020-12-12 21:00:18 -0800

    Added levels and progression

---
+++ Scripts/LevelOne.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+func _physics_process(delta):
+	var scoreTracker = get_node("ScoreTracker")
+	if scoreTracker.complete == 1:
+		get_tree().change_scene("res://Scenes/LevelTwo.tscn")
--- Scripts/ScoreTracker.gd
+++ Scripts/ScoreTracker.gd
@@ -2,10 +2,15 @@ extends Node2D
 
 var killCount = 0
 var animalCount = 0
-
+var killNeed = 0
+var animalNeed = 0
+var wait = 0
+var complete = 0
 
 func _physics_process(delta):
-	if killCount == 3 && animalCount == 2:
-		killCount = 0
-		animalCount = 0
-		print(killCount)
+	wait = wait + 1
+	if wait > 100:
+		if killCount == killNeed && animalCount == animalNeed:
+			killCount = 0
+			animalCount = 0
+			complete = 1
